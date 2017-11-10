16240447703e9fad0af7331175ca0d289f7549c0
Yuri-Popov <popovsqa@gmail.com>
2017-11-09 19:34:24 -0800

    2.7

--- HRM/src/test/java/popov/sqa/EmCreationTest.java
+++ HRM/src/test/java/popov/sqa/EmCreationTest.java
@@ -2,47 +2,60 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
-import static org.testng.Assert.*;
 
 import java.util.concurrent.TimeUnit;
-import java.util.Date;
-import java.io.File;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.*;
-import static org.openqa.selenium.OutputType.*;
 
-public class EmCreationTest {
-    ChromeDriver wd;
+import org.openqa.selenium.*;
+import popov.sqa.EmData;
+
+public class EmCreationTest { // create object of the class 'EmCreationTest'
+    ChromeDriver wd; // WD is an attribute of the object 'EmCreationTest'
     
     @BeforeMethod
     public void setUp() throws Exception {
-        wd = new ChromeDriver();
+        wd = new ChromeDriver(); // initialization attribute of the object 'EmCreationTest'. DW is an attribute of the object
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         wd.get("http://local.school.portnov.com:9595/symfony/web/index.php/auth/login");
+        login("admin", "password");
+    }
+
+    private void login(String username, String password) {
         wd.findElement(By.id("txtUsername")).click();
         wd.findElement(By.id("txtUsername")).clear();
-        wd.findElement(By.id("txtUsername")).sendKeys("admin");
+        wd.findElement(By.id("txtUsername")).sendKeys(username);
         wd.findElement(By.id("txtPassword")).click();
         wd.findElement(By.id("txtPassword")).clear();
-        wd.findElement(By.id("txtPassword")).sendKeys("password");
+        wd.findElement(By.id("txtPassword")).sendKeys(password);
         wd.findElement(By.id("btnLogin")).click();
     }
-    
+
     @Test
     public void testEmCreation() {
-        wd.findElement(By.id("btnAdd")).click();
-        wd.findElement(By.id("firstName")).click();
-        wd.findElement(By.id("firstName")).clear();
-        wd.findElement(By.id("firstName")).sendKeys("wsdf");
-        wd.findElement(By.id("lastName")).click();
-        wd.findElement(By.id("lastName")).clear();
-        wd.findElement(By.id("lastName")).sendKeys("gfde");
-        wd.findElement(By.id("btnSave")).click();
-        wd.findElement(By.linkText("PIM")).click();
+        gotoPIMPage();
+        initEmCreation();
+        fillEmForm(new EmData("wsdf", "gfde"));
+        gotoPIMPage();
     }
     
+
+    private void fillEmForm(EmData emData) {
+        wd.findElement(By.id("firstName")).click();
+        wd.findElement(By.id("firstName")).clear();
+        wd.findElement(By.id("firstName")).sendKeys(emData.getFirstName());
+        wd.findElement(By.id("lastName")).click();
+        wd.findElement(By.id("lastName")).clear();
+        wd.findElement(By.id("lastName")).sendKeys(emData.getLastName());
+        wd.findElement(By.id("btnSave")).click();
+    }
+
+    private void gotoPIMPage() {
+        wd.findElement(By.id("menu_pim_viewPimModule")).click();
+    }
+
+    private void initEmCreation() {
+        wd.findElement(By.id("btnAdd")).click();
+    }
+
     @AfterMethod
     public void tearDown() {
         wd.quit();
---
+++ HRM/src/test/java/popov/sqa/EmData.java
@@ -0,0 +1,19 @@
+package popov.sqa;
+
+public class EmData {
+   private final String firstName;
+   private final String lastName;
+
+   public EmData(String firstName, String lastName) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+   }
+
+   public String getFirstName() {
+      return firstName;
+   }
+
+   public String getLastName() {
+      return lastName;
+   }
+}
